ae8c14e5c631ecdbd06a1b0140287a89b99353a7
osvaldsoza <osvald.soza@gmail.com>
2023-11-03 19:47:15 -0300

    refactory main and router

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/osvaldsoza/gooportunities/config"
 	"github.com/osvaldsoza/gooportunities/router"
 )
 
+var (
+	loggeer *config.Logger
+)
+
 func main() {
+	loggeer = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		loggeer.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
